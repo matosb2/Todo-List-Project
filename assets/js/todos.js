8660372df1c65c872d5bbf937fb845f5d14392e5
Brian Matos <justmats@gmail.com>
2018-04-11 21:34:59 -0400

    Change color and add linethrough on click when completing item on list

---
+++ assets/js/todos.js
@@ -0,0 +1,21 @@
+// Check off specific Todos by clicking
+$("li").click(function () {
+    // if li color is gray
+    if ($(this).css("color") === "rgb(128, 128, 128)") {
+        // turn it black and remove line-through
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        });
+    } else {
+        // turn the color gray and add line-through
+        $(this).css({
+            color: "gray",
+            textDecoration: "line-through"
+        });
+    }
+
+    // Super easier shortcut *******
+    // $(this).toggleClass("completed");
+
+});
