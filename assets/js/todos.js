83a55a786001353d343b9752b2c84a6bfea07d99
Brian Matos <justmats@gmail.com>
2018-04-11 22:59:33 -0400

    Use .on() jquery method instead of .click() to better functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,28 +1,38 @@
 // Check off specific Todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
     // if li color is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        // turn it black and remove line-through
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
-    } else {
-        // turn the color gray and add line-through
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
+    // if ($(this).css("color") === "rgb(128, 128, 128)") {
+    //     // turn it black and remove line-through
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     });
+    // } else {
+    //     // turn the color gray and add line-through
+    //     $(this).css({
+    //         color: "gray",
+    //         textDecoration: "line-through"
+    //     });
+    // }
 
     // Super easier shortcut *******
-    // $(this).toggleClass("completed");
+    $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
 })
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
