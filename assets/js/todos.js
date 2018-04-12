26f8b1de9faa692e80238c89408d9de2e0252a57
Brian Matos <justmats@gmail.com>
2018-04-11 22:32:36 -0400

    Add fadeout

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,5 +17,12 @@ $("li").click(function () {
 
     // Super easier shortcut *******
     // $(this).toggleClass("completed");
-
 });
+
+// Click on X to delete Todo
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
