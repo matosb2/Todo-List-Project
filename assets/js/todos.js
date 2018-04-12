ab2f39560bb1d833d669045f6d8ed959afcf193d
Brian Matos <justmats@gmail.com>
2018-04-12 01:26:28 -0400

    Replace X's with icon

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -20,19 +20,20 @@ $("ul").on("click", "li", function () {
 });
 
 // Click on X to delete Todo
-$("ul").on("click", "span", function(event) {
-    $(this).parent().fadeOut(500, function() {
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
 })
 
-$("input[type='text']").keypress(function(event) {
-    if(event.which === 13) {
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
         // grabbing new todo text from input
         var todoText = $(this).val();
         $(this).val("");
         // create a new li and add to ul
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class="
+            fa fa - trash "></i></span> " + todoText + "</li>");
     }
 });
